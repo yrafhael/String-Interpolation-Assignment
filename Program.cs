686e329d225c1d131b30b3370adb82dbddae6881
Yose Rafhael <yrafhael@my.wctc.edu>
2024-02-15 00:56:35 -0600

    [1.02] Creating prompt mesagge -creating task for user -reading user input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("1. Create data file");
+        Console.WriteLine("2. Parse data file");
+
+        int choice;
+        // Reading user input
+        if (!int.TryParse(Console.ReadLine(), out choice))
+        {
+            Console.WriteLine("Invalid choice.");
+            return;
+        }
+    }
+}
