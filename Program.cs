2ad9953e976befd1c914fbaadae62a48e868ee21
Yose Rafhael <yrafhael@my.wctc.edu>
2024-02-15 01:14:41 -0600

    [1.03] CreateDataFile() -asking user number of weeks -creating file to generate the data -giving back random date (multiply by 2)

--- Program.cs
+++ Program.cs
@@ -14,4 +14,30 @@
             return;
         }
     }
+
+
+    static void CreateDataFile()
+    {
+        Console.Write("Enter number of weeks: ");
+        int weeks = int.Parse(Console.ReadLine());
+
+        // write the data into a file
+        using (StreamWriter writer = new StreamWriter("dataGenerate.txt"))
+        {
+            for (int i = 0; i < weeks; i++)
+            {
+                // giving back date for each week
+                writer.WriteLine($"Week of {DateTime.Today.AddDays(7 * i):MMM, dd, yyyy}");
+                // returning or generate random data for each day of the week -multiples of 2
+                for (int j = 0; j < 7; j++)
+                {
+                    writer.Write($"{(j + 1) * 2},");
+                }
+            
+                writer.WriteLine(); 
+            }
+        }
+
+        Console.WriteLine("Data file created.");
+    }
 }
